a6cb16eed0810f8e15197267669e10300f025ebd
Jiggly Balls <jigglyballs9000@gmail.com>
2024-08-02 17:01:37 +0400

    Man

---
+++ main.py
@@ -0,0 +1,23 @@
+print(
+    """
+I was making a game in pygame for my gfs birthday and a few days before her birthday
+(which is on Aug 10th) I told her I'm making something special (for her birthday).
+
+But she was devastated.
+
+Because on August 1st (gf day) I made her the tower from tangled (her fav disney movie)
+and she felt so bad for me cause I did so much for her (It only took me a few hours)
+but she felt so bad after seeing what I made for her that she started to cry (idk if she
+was crying out of happiness or pity).
+
+And now here I am with a half finished game confused if I should finish it and show it
+to her or not. I even insisted that I made it out of love but she felt heart broken 
+when I told that to her.
+
+Man.
+
+Women are so complicated.
+
+I don't even know what the fuck am I supposed to do for her birthday.
+"""
+)
